98f2320d4fcb3be4898bf2fcac9304de9efdcf4f
Betsy <aby_7710@hotmail.com>
2022-10-03 15:53:49 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import styled from '@emotion/styled'
+import ImagenCripti from './img/imagen-criptos.png'
+import Formulario from './components/Formulario'
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+`
+
+const Imagen = styled.img`
+  max-width: 400%;
+  margin: 100px auto 0 auto;
+  width: 80%;
+  display: block;`
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #FFF;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`
+
+function App() {
+
+  return (
+  <Contenedor>
+    <Imagen 
+      src={ImagenCripti} 
+      alt="imagen criptos"
+    />
+    <div>
+      <Heading>Cotiza criptomonedas al instante</Heading>
+      <Formulario/>
+    </div>
+  </Contenedor>
+  )
+}
+
+export default App
